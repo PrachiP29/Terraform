298864e89ac58f33ec34d2792fd6587027f80ad8
PrachiP29 <86628177+PrachiP29@users.noreply.github.com>
2022-11-17 15:30:16 +0530

    Update newS3_CRR.tf

--- newS3_CRR.tf
+++ newS3_CRR.tf
@@ -1,15 +1,34 @@
- provider "aws" {
+provider "aws" {
   region = "eu-west-1"
 }
 
 provider "aws" {
   alias  = "central"
   region = "eu-central-1"
-} 
+}
 
 resource "aws_s3_bucket" "bucketone" {
-  bucket   = "p-tf-test-bucket-01"
-  acl      = "private"
+  bucket = "p-tf-test-bucket-01"
+  acl    = "private"
+
+  #---- lifecycle rule for 1st bucket -----#
+  lifecycle_rule {
+    id      = "archive"
+    enabled = true
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+    transition {
+      days          = 60
+      storage_class = "GLACIER"
+    }
+  }
+  versioning {
+    enabled = true
+  }
+  tags = { Environment : "Dev" }
+
 }
 
 # Enable versioning for bucket one
@@ -24,16 +43,36 @@ resource "aws_s3_bucket" "buckettwo" {
   provider = aws.central
   bucket   = "p-tf-test-bucket-02"
   acl      = "private"
+
+  #---- lifecycle rule for 1st bucket -----#
+  lifecycle_rule {
+    id      = "archive"
+    enabled = true
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+    transition {
+      days          = 60
+      storage_class = "GLACIER"
+    }
+  }
+  versioning {
+    enabled = true
+  }
+  tags = { Environment : "Dev" }
+
+
 }
 # Enable versioning for bucket two
 resource "aws_s3_bucket_versioning" "buckettwo" {
   provider = aws.central
-  bucket = aws_s3_bucket.buckettwo.id
+  bucket   = aws_s3_bucket.buckettwo.id
   versioning_configuration {
     status = "Enabled"
   }
 }
-  
+
 
 # CREATE IAM ROLE FOR THE REPLICATION RULE
 resource "aws_iam_role" "replication01" {
@@ -57,7 +96,7 @@ POLICY
 }
 
 resource "aws_iam_policy" "replication01" {
-  name = "tf-iam-role-policy-replication-bucket-01"
+  name   = "tf-iam-role-policy-replication-bucket-01"
   policy = <<POLICY
 {
   "Version": "2012-10-17",
@@ -105,8 +144,8 @@ resource "aws_iam_role_policy_attachment" "replication01" {
 
 # CREATE IAM ROLE FOR THE REPLICATION RULE
 resource "aws_iam_role" "replication02" {
-  name = "tf-iam-role-replication-bucket-02"
-
+  name               = "tf-iam-role-replication-bucket-02"
+  provider           = aws.central
   assume_role_policy = <<POLICY
 {
   "Version": "2012-10-17",
@@ -125,9 +164,9 @@ POLICY
 }
 
 resource "aws_iam_policy" "replication02" {
-  name = "tf-iam-role-policy-replication-bucket-02"
-
-  policy = <<POLICY
+  name     = "tf-iam-role-policy-replication-bucket-02"
+  provider = aws.central
+  policy   = <<POLICY
 {
   "Version": "2012-10-17",
   "Statement": [
@@ -167,44 +206,43 @@ POLICY
 }
 
 resource "aws_iam_role_policy_attachment" "replication02" {
+  provider   = aws.central
   role       = aws_iam_role.replication02.name
   policy_arn = aws_iam_policy.replication02.arn
 }
 
- # REPLICATION CONFIGURATION for one
+# REPLICATION CONFIGURATION for one
 
 resource "aws_s3_bucket_replication_configuration" "bucket01_to_bucket02" {
   # Must have bucket versioning enabled first
-  depends_on = [aws_s3_bucket_versioning.bucketone]
+  depends_on = [aws_s3_bucket_versioning.bucketone, aws_s3_bucket.bucketone]
 
   role   = aws_iam_role.replication01.arn
   bucket = aws_s3_bucket.bucketone.id
 
   rule {
     id = "bucket-01"
-
-    filter {
-      prefix = "bucket-one"
-    }
-
     status = "Enabled"
 
     destination {
       bucket        = aws_s3_bucket.buckettwo.arn
       storage_class = "STANDARD"
+      //replica_kms_key_id = aws_kms_key.buckettwo.arn
     }
+   /*  source_selection_criteria {
+        sse_kms_encrypted_objects {
+          enabled = "true"
+        }
+      } */
 
-     delete_marker_replication {
-  status = "Enabled"
-   }
   }
-} 
+}
 
- # REPLICATION CONFIG FOR TWO
+# REPLICATION CONFIG FOR TWO
 resource "aws_s3_bucket_replication_configuration" "bucket02_to_bucket01" {
   provider = aws.central
   # Must have bucket versioning enabled first
-  depends_on = [aws_s3_bucket_versioning.buckettwo]
+  depends_on = [aws_s3_bucket_versioning.buckettwo, aws_s3_bucket.buckettwo]
 
   role   = aws_iam_role.replication02.arn
   bucket = aws_s3_bucket.buckettwo.id
@@ -212,20 +250,16 @@ resource "aws_s3_bucket_replication_configuration" "bucket02_to_bucket01" {
   rule {
     id = "bucket02"
 
-    filter {
-      prefix = "bucket-two"
-    }
 
     status = "Enabled"
 
     destination {
       bucket        = aws_s3_bucket.bucketone.arn
       storage_class = "STANDARD"
+      //replica_kms_key_id = aws_kms_key.bucketone.arn
     }
 
-     delete_marker_replication {
-  status = "Enabled"
-   }
+
   }
 }
   
